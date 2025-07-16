25fb49590411edf9f11e3e797a884728e17069d3
fjer <fhm.pribadi@gmail.com>
2025-07-16 19:44:36 +0700

    intital commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,10 @@
+<x-layout :title="$title">
+
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <!-- Your content -->
+        </div>
+    </main>
+
+</x-layout>
+
